b067bcc83efe23ea9ca0f9f662d762330ca84bea
A-m-a-n-PB <aman.kr.singh@pickleball.com>
2024-12-10 19:48:22 +0530

    todays commit -2

---
+++ Docsearch.jsx
@@ -0,0 +1,15 @@
+import { DocSearch } from '@docsearch/react';
+
+import '@docsearch/css';
+
+function App() {
+  return (
+    <DocSearch
+      appId="XUFA6OX4Z8"
+      indexName="pickleball"
+      apiKey="017e25ae6962d6f5a6ce8e9fda2f10ec"
+    />
+  );
+}
+
+export default App;
